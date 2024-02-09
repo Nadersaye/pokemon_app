04b79ce7c627b4e6caf7cf70065443903f84afc1
Nader <ns1568@fayoum.edu.eg>
2024-02-09 17:56:36 +0200

    Add PokemonDetailsViewBody widget

---
+++ lib/features/home/presentaion/views/widgets/pokemon_details_view_body.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PokemonDetailsViewBody extends StatelessWidget {
+  const PokemonDetailsViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: <Widget>[
+        Text('PokemonDetailsViewBody'),
+      ],
+    );
+  }
+}
