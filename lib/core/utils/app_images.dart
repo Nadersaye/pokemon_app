19dc066125b6719ce593e8df8bd4d12cf1f2208c
Nader <ns1568@fayoum.edu.eg>
2024-02-09 17:54:58 +0200

    Add app_images.dart with image assets

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,13 @@
+class Assets {
+  Assets._();
+
+  /// Assets for imageBackgroundHome
+  /// assets/images/background_home_image.jpg
+  static const String imageBackgroundHome =
+      "assets/images/background_home_image.jpg";
+
+  /// Assets for imageBackgroundOnboarding
+  /// assets/images/background_onboarding_image.PNG
+  static const String imageBackgroundOnboarding =
+      "assets/images/background_onboarding_image.PNG";
+}
