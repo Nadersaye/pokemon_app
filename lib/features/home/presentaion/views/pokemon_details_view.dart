ddfc92315fe89fd9139c2b6f447863dc11a21dff
Nader <ns1568@fayoum.edu.eg>
2024-02-12 14:36:10 +0200

    Add PokemonDetailsEntity to PokemonDetailsView constructor

--- lib/features/home/presentaion/views/pokemon_details_view.dart
+++ lib/features/home/presentaion/views/pokemon_details_view.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:pokemon_app/features/home/domain/entities/pokemon_details_entity.dart';
 import 'widgets/pokemon_details_view_body.dart';
 
 class PokemonDetailsView extends StatelessWidget {
-  const PokemonDetailsView({super.key});
+  final PokemonDetailsEntity pokemonDetails;
+  const PokemonDetailsView({super.key, required this.pokemonDetails});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: PokemonDetailsViewBody());
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("pokemon"),
+        ),
+        body: PokemonDetailsViewBody(
+          pokemon: pokemonDetails,
+        ));
   }
 }
