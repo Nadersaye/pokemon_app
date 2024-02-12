1a99f8e15d4ce47e8d9afd0091af8fcaed9eeab8
Nader <ns1568@fayoum.edu.eg>
2024-02-12 14:31:25 +0200

    Add AppColors class with predefined color constants

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const performanceTextColor = Color(0xff8c8c8c);
+  static const linearIndicatorBackgroundColor = Color(0xffcccccc);
+  static const hpColor = Color(0xfff54336);
+  static const attackColor = Color(0xff47a6ff);
+  static const defenceColor = Color(0xffff94d1);
+  static const specialAttackColor = Color(0xff9bfc72);
+  static const specialDefenseColor = Color(0xff9f82ff);
+  static const speedColor = Color(0xffffe482);
+}
