56e5159fa7ed35a6a79c338e268e66048d561169
Nader <ns1568@fayoum.edu.eg>
2024-02-12 14:39:39 +0200

    Add device preview and initialize Hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,23 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
-import 'package:pokemon_app/features/onboarding/presentation/views/onboarding_view.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:pokemon_app/core/utils/app_router.dart';
+import 'package:pokemon_app/core/utils/constants.dart';
+import 'package:pokemon_app/core/utils/service_locator.dart';
+import 'package:pokemon_app/features/home/domain/entities/pokemon_details_entity.dart';
+import 'package:pokemon_app/features/home/domain/entities/pokemon_entity.dart';
 
-void main() {
-  runApp(const PokemonApp());
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(PokemonEntityAdapter());
+  Hive.registerAdapter(PokemonDetailsEntityAdapter());
+  ServiceLocator.init();
+  await Hive.openBox<PokemonEntity>(kPokemonsBox);
+  await Hive.openBox<PokemonDetailsEntity>(kPokemonDetailsBox);
+  runApp(DevicePreview(
+    enabled: true,
+    builder: (context) => const PokemonApp(),
+  ));
 }
 
 class PokemonApp extends StatelessWidget {
@@ -10,16 +25,18 @@ class PokemonApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: 'Pokemon App',
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       theme: ThemeData(
         primarySwatch: Colors.purple,
         buttonTheme: const ButtonThemeData(
           buttonColor: Colors.red,
         ),
       ),
-      home: const OnboardingView(),
+      routerConfig: AppRouter.router,
     );
   }
 }
