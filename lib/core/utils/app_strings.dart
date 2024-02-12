3d68cd59af6ef10a12f644d2bbdade96546758be
Nader <ns1568@fayoum.edu.eg>
2024-02-12 14:31:42 +0200

    Add AppStrings class with Pokemon-related strings

---
+++ lib/core/utils/app_strings.dart
@@ -0,0 +1,12 @@
+abstract class AppStrings {
+  static const attack = 'atk';
+  static const hp = 'hp';
+  static const defense = 'def';
+  static const specialAttack = 'spa';
+  static const specialDefense = 'spd';
+  static const speed = 'exp';
+  static const welcomeViewButtonText = 'see the list of pokemons';
+  static const welcomeViewTitle = 'welcome !';
+  static const homePokemonViewTitle = 'List of Pokemons';
+  static const homepokemonViewButtonText = 'PokeApi webpage';
+}
