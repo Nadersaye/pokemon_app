bc22aa8ec743380067b05a86e0edc83c6b33cdc9
Nader <ns1568@fayoum.edu.eg>
2024-02-12 14:35:48 +0200

    Add PerformanceDetailsEntity class

---
+++ lib/features/home/domain/entities/performance_details_entity.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PerformanceDetailsEntity {
+  final String title;
+  final int value;
+  final Color color;
+
+  PerformanceDetailsEntity(
+      {required this.title, required this.value, required this.color});
+}
