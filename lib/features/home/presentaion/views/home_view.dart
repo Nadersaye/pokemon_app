8cc72d309737dc769780cd14d0a1fae03b3f4922
Nader <ns1568@fayoum.edu.eg>
2024-02-14 15:31:39 +0200

    Refactor HomeView to use Flutter Riverpod for state management

--- lib/features/home/presentaion/views/home_view.dart
+++ lib/features/home/presentaion/views/home_view.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
-import '../../domain/entities/pokemon_details_entity.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../view model/pokemon_details_provider/fetch_pokemon_details_provider.dart';
 import 'widgets/home_view_body.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends ConsumerStatefulWidget {
   const HomeView({super.key});
+
+  @override
+  ConsumerState<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<HomeView> {
+  @override
+  void initState() {
+    ref.read(fetchPokemonDetailsProvider.notifier).fetchPokemonDetails();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,22 +23,7 @@ class HomeView extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Pokemon App'),
       ),
-      body: HomeViewBody(
-        pokemon: PokemonDetailsEntity(
-            pokemonTypes: ['grass', 'water', 'fire'],
-            pokemonId: 3,
-            pokemonName: 'khgjhgjhg',
-            pokemonImageUrl:
-                'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/3.png',
-            pokemonHeight: 30,
-            pokemonWeight: 90,
-            hp: 40,
-            attack: 60,
-            defense: 50,
-            specialAttack: 65,
-            specialDefense: 150,
-            speed: 80),
-      ),
+      body: const HomeViewBody(),
     );
   }
 }
