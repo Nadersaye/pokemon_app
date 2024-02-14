f8b1a8c7a0f200490b838f2541350885b22b7c9b
Nader <ns1568@fayoum.edu.eg>
2024-02-14 15:27:58 +0200

    Add FetchPokemonDetailsProvider and FetchPokemonDetailsNotifier

---
+++ lib/features/home/presentaion/view model/pokemon_details_provider/fetch_pokemon_details_provider.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pokemon_app/core/utils/service_locator.dart';
+import '../../../domain/use_cases/fetch_pokemons_details_use_case.dart';
+import 'fetch_pokemon_details_states.dart';
+
+final fetchPokemonDetailsProvider = StateNotifierProvider.autoDispose<
+        FetchPokemonDetailsNotifier, FetchPokemonDetailsState>(
+    (ref) => FetchPokemonDetailsNotifier(
+        ref.watch(fetchPokemonsDetailsUseCaseProvider)));
+
+class FetchPokemonDetailsNotifier
+    extends StateNotifier<FetchPokemonDetailsState> {
+  final FetchPokemonsDetailsUseCase _fetchPokemonDetailsUseCase;
+
+  FetchPokemonDetailsNotifier(this._fetchPokemonDetailsUseCase)
+      : super(FetchPokemonDetailsInitial());
+
+  Future fetchPokemonDetails({int pageNumber = 0}) async {
+    /*if (pageNumber == 0) {
+      state = FetchPokemonDetailsLoading();
+    } else {
+      state = FetchPokemonDetailsPaginationLoading();
+    }*/
+    if (pageNumber > 0) {
+      state = FetchPokemonDetailsPaginationLoading();
+    }
+    final result = await _fetchPokemonDetailsUseCase.call(pageNumber);
+    result.fold(
+      (failure) {
+        if (pageNumber == 0) {
+          state = FetchPokemonDetailsError(errorMessage: failure);
+        } else {
+          state = FetchPokemonDetailsPaginationError(errorMessage: failure);
+        }
+      },
+      (pokemons) => state = FetchPokemonDetailsLoaded(pokemons: pokemons),
+    );
+  }
+}
