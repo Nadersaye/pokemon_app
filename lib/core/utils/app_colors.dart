cd2a0ad4acbd578ad90055b18f5ee7909de72787
Nader <ns1568@fayoum.edu.eg>
2024-02-14 21:01:24 +0200

    Add new colors to AppColors

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 abstract class AppColors {
+  static const primaryColor = Color(0xff6716f7);
   static const performanceTextColor = Color(0xff8c8c8c);
   static const linearIndicatorBackgroundColor = Color(0xffcccccc);
   static const hpColor = Color(0xfff54336);
@@ -9,4 +10,11 @@ abstract class AppColors {
   static const specialAttackColor = Color(0xff9bfc72);
   static const specialDefenseColor = Color(0xff9f82ff);
   static const speedColor = Color(0xffffe482);
+  static const blackColor = Colors.black;
+  static const black12Color = Colors.black12;
+  static const whiteColor = Colors.white;
+  static const greyColor = Colors.grey;
+  static const redColor = Colors.red;
+  static const grey38Color = Color(0xff616161);
+  static const gray55Color = Color(0xff8c8c8c);
 }
