2e3bb26f5af3dac35a23b6a8a3f492b9c454f81e
Nader <ns1568@fayoum.edu.eg>
2024-02-14 21:09:53 +0200

    Remove unused code and update MaterialApp configuration

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:pokemon_app/core/utils/app_router.dart';
 import 'package:pokemon_app/core/utils/constants.dart';
 import 'package:pokemon_app/features/home/domain/entities/pokemon_details_entity.dart';
 import 'package:pokemon_app/features/home/domain/entities/pokemon_entity.dart';
-
 import 'core/utils/simple_provider_observer.dart';
 
 void main() async {
@@ -19,7 +18,7 @@ void main() async {
     observers: [SimpleProviderObserver()],
   );
   runApp(DevicePreview(
-    enabled: true,
+    enabled: false,
     builder: (context) => ProviderScope(
       parent: container,
       child: const PokemonApp(),
@@ -34,15 +33,8 @@ class PokemonApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      title: 'Pokemon App',
       locale: DevicePreview.locale(context),
       builder: DevicePreview.appBuilder,
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        buttonTheme: const ButtonThemeData(
-          buttonColor: Colors.red,
-        ),
-      ),
       routerConfig: AppRouter.router,
     );
   }
